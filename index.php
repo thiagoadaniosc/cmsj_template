320ffb7f6b4d247d6d1995a8de565ed106ab424a
thiagoadaniosc <thiagoadaniosc@gmail.com>
2017-09-30 06:32:09 -0300

    First

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/template.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script type="text/javascript" href="js/bootstrap.min.js"></script>
+	<title>CMSJ - INTRANET</title>
+</head>
+<body>
+
+	<div id="wrapper">
+
+		<header class="container header col-lg-12 fixed-top">
+			<section class="header-logo col-lg-3">
+				<a href=""><img src="imgs/sao-jose-logo.png" class="img-responsive col-lg-12"></a>
+			</section>
+			<section class="menu-top collapse navbar-collapse">
+				<ul class="navbar nav nav-pills">
+					<li class="nav-item"><a href="">PRINCIPAL</a></li>
+					<li class="nav-item"><a href="">NOTICIAS</a></li>
+					<li class="nav-item"><a href="">INFORMATIVOS</a></li>
+					<li class="nav-item"><a href="">SUPORTE</a></li>
+					<li class="nav-item"><a href="">GUIA TELEFÔNICO</a></li>
+					<li class="nav-item">
+						<form class="form-group form-inline">
+							<input class="form-control input-busca" type="text" name="" placeholder="Pesquisar...">
+							<button class="btn btn-default btn-busca form-control"><i class="fa fa-search"></i></button>
+						</form>
+					</li>
+				</ul>
+			</section>
+		</header>
+		<!-- Fim Header -->
+
+
+		<main class="conteudo row col-lg-12">
+			<section class="conteudo-principal">
+				<h1>Intranet CMSJ</h1>
+			</section>
+
+		</main> 
+
+	</div>
+	<!-- Inicio Footer -->
+	<footer class="footer">
+		<section class="container">
+			<section class="social-networks">
+				<h2>REDES SOCIAIS</h2>
+				<section class="social-networks-icons">
+					<img src="/cmsj/public/assets/images/facebook-icon.png" class="img-responsive">
+					<img src="/cmsj/public/assets/images/twitter-icon.png" class="img-responsive">
+					<img src="/cmsj/public/assets/images/instagram-icon.png" class="img-responsive">
+					<img src="/cmsj/public/assets/images/youtube-icon.png" class="img-responsive">
+				</section>
+			</section>
+			<img src="/cmsj/public/assets/images/sao-jose-footer.png" class="img-responsive img-footer-logo">
+			<nav>
+				<ul class="nav nav-stacked nav-pills">
+					<li>PRAÇA ARNOLDO DE SOUZA, 38 - CENTRO - SÃO JOSÉ - SC</li>
+					<li>CEP: 88103-005</li>
+					<li>TELEFONE: (48) 3029-1321</li>
+					<li>E-mail: ouvidoria@cmsj.sc.gov.br</li>
+				</ul>
+			</nav>
+		</section>
+	</footer>
+
+	<!-- Fim Footer -->
+
+</body>
+</html>
