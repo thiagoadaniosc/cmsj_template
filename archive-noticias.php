6a70c55c5cda4942b28b4278c2d67d1c41b5a7a5
thiagoadaniosc <thiagoadaniosc@gmail.com>
2017-11-07 15:56:49 -0200

    Alteração de template archive noticias

--- archive-noticias.php
+++ archive-noticias.php
@@ -10,11 +10,16 @@
 		<?php while (have_posts()):
 		the_post();
 		$post_id = get_the_ID();
+
+		$post_thumbnail_url = /*get_template_directory_uri() . */'https://s.profissionaisti.com.br/wp-content/uploads/2014/02/gerenciamento-aquisicoes-projetos-ti.jpg';
+		if (get_the_post_thumbnail_url($post_id )): 
+			$post_thumbnail_url = get_the_post_thumbnail_url($post_id ,'full'); 
+		endif; 
 		?>
 		<div class="col-lg-3" style="padding: 2px">
 			<div class="thumbnail col-lg-12" style="border-radius: 0;">
 				<a class="home-last-news-link" href="<?= get_post_permalink(); ?>">
-					<figure class="col-lg-12">
+					<figure class="col-lg-12"">
 						<span class="home-last-news-tag">
 
 							<?= get_the_terms($post_id, 'post_tag')[0]->name; ?>
@@ -24,8 +29,8 @@
 							<i class="fa fa-calendar"></i>
 							<?php echo get_the_date( 'd/m/Y' ); ?>
 						</span>
-
-						<img src="https://s.profissionaisti.com.br/wp-content/uploads/2014/02/gerenciamento-aquisicoes-projetos-ti.jpg" class="img-responsive">
+			
+						<img src="<?= $post_thumbnail_url; ?>" class="img-responsive">
 
 						<h2 class="text-center news-title col-lg-12" style=""><?php the_title(); ?></h2>
 					</figure>
